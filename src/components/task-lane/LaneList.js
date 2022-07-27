e1d732f020af59bfbcdb9010b03e4732e0730b01
saifilwebisoft <saifilmomin4@gmail.com>
2022-07-26 20:31:34 -0400

    fixed crashing issues on drag and drop

--- src/components/task-lane/LaneList.js
+++ src/components/task-lane/LaneList.js
@@ -7,44 +7,50 @@ import Lane from './Lane'
 const LaneList = ({ filteredLanes, setFilteredLanes, setLanes }) => {
 
     const handleDragEnd = (result) => {
-        setFilteredLanes(UpdatedLanes(result));
+        const updated = UpdatedLanes(result);
+        if (updated) {
+            setFilteredLanes(UpdatedLanes(result));
+        }
     }
 
     const UpdatedLanes = (result) => {
-        const sourceid = parseInt(result.source.droppableId);
-        const destionationid = parseInt(result.destination.droppableId);
-        const destinationindex = parseInt(result.destination.index);
-        const sourceindex = parseInt(result.source.index);
-        if (sourceid !== destionationid) {
-            let removedTask = null;
-            filteredLanes = filteredLanes.map(lane => {
-                if (lane.id === sourceid) {
-                    removedTask = lane.tasks.splice(sourceindex, 1);
-                    removedTask.laneid = destionationid;
+        if (result && result.destination !== null) {
+            const sourceid = parseInt(result.source.droppableId);
+            const destionationid = parseInt(result.destination.droppableId);
+            const destinationindex = parseInt(result.destination.index);
+            const sourceindex = parseInt(result.source.index);
+            if (sourceid !== destionationid) {
+                let removedTask = null;
+                filteredLanes = filteredLanes.map(lane => {
+                    if (lane.id === sourceid) {
+                        removedTask = lane.tasks.splice(sourceindex, 1);
+                        removedTask.laneid = destionationid;
+                        return lane;
+                    }
                     return lane;
-                }
-                return lane;
-            })
-            return filteredLanes.map(lane => {
-                if (lane.id === destionationid) {
-                    let copiedtasks = lane.tasks;
-                    copiedtasks.splice(destinationindex, 0, removedTask);
-                    return { ...lane, tasks: [].concat.apply([], copiedtasks) };
-                }
-                return lane;
-            });
-        } else {
-            return filteredLanes.map(lane => {
-                if (lane.id === destionationid) {
-                    let copiedtasks = lane.tasks;
-                    const removedTask = copiedtasks.splice(sourceindex, 1);
-                    copiedtasks.splice(destinationindex, 0, removedTask);
-                    console.log(copiedtasks);
-                    return { ...lane, tasks: [].concat.apply([], copiedtasks) };
-                }
-                return lane;
-            });
+                })
+                return filteredLanes.map(lane => {
+                    if (lane.id === destionationid) {
+                        let copiedtasks = lane.tasks;
+                        copiedtasks.splice(destinationindex, 0, removedTask);
+                        return { ...lane, tasks: [].concat.apply([], copiedtasks) };
+                    }
+                    return lane;
+                });
+            } else {
+                return filteredLanes.map(lane => {
+                    if (lane.id === destionationid) {
+                        let copiedtasks = lane.tasks;
+                        const removedTask = copiedtasks.splice(sourceindex, 1);
+                        copiedtasks.splice(destinationindex, 0, removedTask);
+                        console.log(copiedtasks);
+                        return { ...lane, tasks: [].concat.apply([], copiedtasks) };
+                    }
+                    return lane;
+                });
+            }
         }
+        return null;
     }
 
     return (
