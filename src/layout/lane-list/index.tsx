724aed3c52abf3a11dcc17ad447024b30175678d
saifil <saifilmomin4@gmail.com>
2024-04-11 13:13:19 -0400

    refactored code

--- src/layout/lane-list/index.tsx
+++ src/layout/lane-list/index.tsx
@@ -9,49 +9,50 @@ interface ILaneListProps {
   setLanes: Dispatch<React.SetStateAction<ILane[]>>
 }
 
-const LaneList = ({ filteredLanes, setFilteredLanes, setLanes }: ILaneListProps) => {
-  const handleDragEnd = (result: DropResult) => {
-    const updated = UpdatedLanes(result);
-    if (updated) {
-      setFilteredLanes(updated);
-    }
-  };
-
-  const UpdatedLanes = (result: DropResult) => {
-    if (result && result.destination !== null) {
-      const source_id = result.source.droppableId;
-      const sourceindex = result.source.index;
-      const destionation_id = result.destination?.droppableId;
-      const destination_index = result.destination?.index;
-      let copiedLanes = Array.from(filteredLanes);
-      if (source_id !== destionation_id) {
-        let sourceLane = copiedLanes.find((lane) => lane.id === source_id);
-        if (sourceLane !== undefined && sourceLane !== null) {
-          const [removed] = sourceLane.tasks.splice(sourceindex, 1);
-          return copiedLanes.map((lane) => {
-            if (lane.id === source_id) {
-              return sourceLane ? sourceLane : lane; //work around not ideal should be return sourcelane
-            } else if (lane.id === destionation_id && destination_index !== undefined) {
-              let copiedtasks = Array.from(lane.tasks);
-              copiedtasks.splice(destination_index, 0, removed);
-              return { ...lane, tasks: copiedtasks };
-            }
-            return lane;
-          });
-        }
-      } else {
+const UpdatedLanes = (result: DropResult, filteredLanes: ILane[]) => {
+  if (result && result.destination !== null) {
+    const source_id = result.source.droppableId;
+    const sourceindex = result.source.index;
+    const destionation_id = result.destination?.droppableId;
+    const destination_index = result.destination?.index;
+    let copiedLanes = Array.from(filteredLanes);
+    if (source_id !== destionation_id) {
+      let sourceLane = copiedLanes.find((lane) => lane.id === source_id);
+      if (sourceLane !== undefined && sourceLane !== null) {
+        const [removed] = sourceLane.tasks.splice(sourceindex, 1);
         return copiedLanes.map((lane) => {
-          if (lane.id === source_id && destination_index !== undefined) {
+          if (lane.id === source_id) {
+            return sourceLane ? sourceLane : lane; //work around not ideal should be return sourcelane
+          } else if (lane.id === destionation_id && destination_index !== undefined) {
             let copiedtasks = Array.from(lane.tasks);
-            const [removedTask] = copiedtasks.splice(sourceindex, 1);
-            copiedtasks.splice(destination_index, 0, removedTask);
+            copiedtasks.splice(destination_index, 0, removed);
             return { ...lane, tasks: copiedtasks };
           }
           return lane;
         });
       }
+    } else {
+      return copiedLanes.map((lane) => {
+        if (lane.id === source_id && destination_index !== undefined) {
+          let copiedtasks = Array.from(lane.tasks);
+          const [removedTask] = copiedtasks.splice(sourceindex, 1);
+          copiedtasks.splice(destination_index, 0, removedTask);
+          return { ...lane, tasks: copiedtasks };
+        }
+        return lane;
+      });
     }
-    return null
+  }
+  return null
+};
+
+
+const LaneList = ({ filteredLanes, setFilteredLanes, setLanes }: ILaneListProps) => {
+  const handleDragEnd = (result: DropResult) => {
+    const updated = UpdatedLanes(result, filteredLanes);
+    if (updated) {
+      setFilteredLanes(updated);
+    }
   };
 
   return (
